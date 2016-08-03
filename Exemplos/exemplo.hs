da63c87a0fb6c6a1c5250b05d696487b94a8f93f
Wellington Schütz <wellington.schu@live.com>
2016-08-03 17:27:01 -0300

    Exemplos e Lista 01

---
+++ Exemplos/exemplo.hs
@@ -0,0 +1,5 @@
+soma x y = x + y -- Função Soma
+inc n = n + 1 -- Função Incremento
+areaTriangulo b h = (b * h) / 2 -- Área Triângulo
+areaTrapezio a b h = (a + b) * h / 2 -- Área Trapézio
+media n1 n2 n3 = (n1 + n2 + n3) / 3 -- Média com 3 números
