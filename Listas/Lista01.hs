5c6faf1308958edc7333e5978e51de66d68bb984
Wellington Schütz <wellington.schu@live.com>
2016-08-06 15:35:12 -0300

    Ec04

--- Listas/Lista01.hs
+++ Listas/Lista01.hs
@@ -2,7 +2,7 @@
 dobro a = a * 2 -- Ex01
 quadruplicar a = dobro (dobro a) -- Ex02
 mult a b c = a * b * c -- Ex03
-distancia a b = b - a -- Ex04
+distancia xa ya xb yb = sqrt ((xb - xa) ^ 2 + (yb - ya) ^ 2) -- Ex04
 hipotenusa a b = sqrt ((a ^ 2) + (b ^ 2)) -- Ex05
 retangulo b h = b * h -- Ex06
 circulo r = pi * (r / 2) -- Ex07
