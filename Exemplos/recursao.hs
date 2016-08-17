a6a9884ec9006b48290ec4b5025314bfa824686e
Wellington Schütz <wellington.schu@live.com>
2016-08-17 11:28:06 -0300

    Exemplos Recursão e Lista 03

---
+++ Exemplos/recursao.hs
@@ -0,0 +1,7 @@
+--Soma recursiva
+soma 1 = 1
+soma n = n + soma (n-1)
+
+--Fatorial
+fat 0 = 1
+fat n = n * fat (n - 1)
---
+++ Listas/Lista03.hs
@@ -0,0 +1,13 @@
+--Ex01
+r 0 = 2
+r x = 2 * r (x - 1) - 4
+
+--Ex02
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n - 1) + fib (n - 2)
+
+--Ex03
+somatorio 0 = 0
+somatorio i = 2 * i ^ 2 + 2 * i + 8 + somatorio (i - 1)
+--Ex04
