cfd8e4733529742c85d2ed86752bc930bb3988be
Wellington Schütz <wellington.schu@live.com>
2016-08-24 08:28:51 -0300

    Lista 03

--- Listas/Lista03.hs
+++ Listas/Lista03.hs
@@ -13,3 +13,18 @@ somatorio i = 2 * i ^ 2 + 2 * i + 8 + somatorio (i - 1)
 
 --Ex04
 power :: Int -> Int -> Int
+power x 1 = 1
+power x 0 = 1
+power x y = power (x)(y - 1) * power (x)(1)
+
+--Exe05
+maxMulComum 1 b c = 1
+maxMulComum a 1 c = 1
+maxMulComum a b 1 = 1
+maxMulComum a b c = if (mod b a == 0 && mod c a == 0)
+    then a
+    else if (a < b && a < c)
+        then maxMulComum (a - 1)(b)(c)
+        else if (b < a && b < c)
+            then maxMulComum (b - 1)(a)(c)
+            else maxMulComum (c - 1)(a)(b)
