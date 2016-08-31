3bc304f4bd5ef1119b03b924c5a3bbbc87e4db4c
Wellington Schütz <wellington.schu@live.com>
2016-08-31 10:03:09 -0300

    Lista04

---
+++ Listas/Lista04.hs
@@ -0,0 +1,19 @@
+--Ex01
+tripla = (1, 'y', "Terceiro")
+select_primeiro (p,_,_) = p
+select_segundo (_,s,_) = s
+select_terceiro (_,_,t) = t
+
+--Ex02
+quadrado :: (Int, Int, Int) -> (Int, Int, Int)
+quadrado (a, b, c) = (a^2, b^2, c^2)
+
+--Ex03
+triangulo :: (Int, Int, Int) -> (String, Int)
+triangulo (a, b, c) = if (a > b + c) || (b > a + c) || (c > a + b)
+    then ("Nao e triangulo", 0)
+    else if a == b && b == c
+        then ("Equilatero", a + b + c)
+        else if (a == b && a /= c && ba /= c) || (a == c && a /= b && c /= b)
+            then ("Isosceles", a + b + c)
+            else ("teste", 0)
